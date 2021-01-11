f38c0d662d085e7c3d723eb99a7e5623c0aba53d
Nicholas Babo <nickbabo7@gmail.com>
2021-01-11 11:22:20 -0300

    Initial Commit

---
+++ Movies/Movies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movies
+//
+//  Created by nicholas.r.babo on 11/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by nicholas.r.babo on 11/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
